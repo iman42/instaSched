316045070f87d15a86130a01a06fab81a26c89b7
Ian Fanselow <if497@nyu.edu>
2017-08-30 17:15:23 -0400

    First

---
+++ resources/views/manage.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Add Account</div>
+                <div class="panel-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    <form class="form-horizontal">
+                      <div class="form-group">
+                        <label for="username" class="col-sm-2 control-label">Username</label>
+                        <div class="col-sm-10">
+                          <input type="text" class="form-control" id="username" name="username" placeholder="Username">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <label for="password" class="col-sm-2 control-label">Password</label>
+                        <div class="col-sm-10">
+                          <input type="password" class="form-control" id="password" name="password" placeholder="Password">
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <div class="col-sm-offset-2 col-sm-10">
+                          <button type="submit" class="btn btn-default">Add Account</button>
+                        </div>
+                      </div>
+                    </form>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class="panel-heading">Your Accounts</div>
+                <div class="panel-body">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <td><strong>#</strong></td>
+                                <td><strong>Username</strong></td>
+                                <td><strong>Followers</strong><td>
+                                <td><strong>Actions</strong></td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <!-- [TODO] -->
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
